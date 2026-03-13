b2800722a47032853561d755334c2683df09151e
Mubelotix <mubelotix@dera.page>
2026-03-13 21:39:54 +0100

    Add rocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::figment::providers::Serialized;
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    let figment = rocket::Config::figment()
+        .merge(Serialized::default("port", 1111));
+
+    rocket::custom(figment).mount("/", routes![index])
 }
